47911c269427daab9bdbe3251b1dbb761f00b114
Na._.kyung <leena3823@gmail.com>
2023-10-19 20:57:49 +0900

    23-10-19 next study

---
+++ nextjscourse/pages/news/1.jsx
@@ -0,0 +1,11 @@
+function NewsPage(){
+    return <Fragment>
+        <h1>The News Page</h1>
+        <ul>
+            <li>NEXTJS is a great framework</li>
+            <li>something else</li>
+        </ul>
+        </Fragment>
+}
+
+export default NewsPage;
